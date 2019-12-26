60677506456fc2edf3666d8b33a4de7461b600aa
Leandro Lourenci <leandro@lourenci.com>
2019-12-25 22:48:51 -0300

    Change entrypoint on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './assets/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
