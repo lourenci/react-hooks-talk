f4b546143669b0b4197560f00d51fc7f2472e4d6
Leandro Lourenci <leandro@lourenci.com>
2020-02-27 14:59:14 -0300

    Configure webpack to load css files

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
   },
   plugins: [new HtmlWebpackPlugin({ template: './assets/index.html' })]
 }
